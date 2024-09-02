2517067f8dc73104a92ae9b81bdefe4289437755
Mike McGowan <mikedmcgowan@gmail.com>
2024-09-02 11:07:54 -0700

    update copy for mobile

--- src/MobileContent.js
+++ src/MobileContent.js
@@ -38,7 +38,7 @@ const MobileContent = () => {
                 <br />
                 <br />
 
-                As a compassionate breakup artist and time bender, I help highly motivated individuals dissolve limiting relationships–to jobs, people, and habits–using personalized, strategic action plans. Together, we’ll apply focused energy and uncommon clarity to rapidly achieve results that could otherwise take years. It’s collaborative time-bending at its best!
+                As a compassionate breakup artist and time bender, I help highly motivated individuals dissolve limiting relationships–to environments, people, and habits–using personalized, strategic action plans. Together, we’ll apply focused energy and uncommon clarity to rapidly achieve results that could otherwise take years. It’s collaborative time-bending at its best!
                 <br />
                 <br />
 
@@ -46,7 +46,7 @@ const MobileContent = () => {
                 <br />
                 <br />
 
-                Breaking free means stepping into a fuller, more expansive life with entirely new levels of success, responsibility, and freedom. Not everyone is ready. But if you’re willing to trade familiarity for focused, fearless expansion, you’re not everyone:
+                Breaking free means stepping into a fuller, more expansive life with entirely new levels of success, agency, and freedom. Not everyone is ready. But if you’re willing to trade familiarity for focused, fearless expansion, you’re not everyone:
                 <br />
                 <br />
 
